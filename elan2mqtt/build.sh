f6d1335def8f9d2b6d479d8851a73032b8fd1a88
pan <pan@email.com>
2023-11-01 11:11:41 +0000

    build

--- elan2mqtt/build.sh
+++ elan2mqtt/build.sh
@@ -1,1 +1,2 @@
-docker build  --build-arg BUILD_FROM=python:3-alpine -t elan2mqtt:latest .
+#docker build  --build-arg BUILD_FROM=python:3-alpine -t elan2mqtt:latest .
+docker build  --build-arg BUILD_FROM=python:3.11 -t elan2mqtt:latest .
