e967fb3c22f5f7c7c3066613df6b71e61884b67a
pan <pan@email.com>
2023-11-01 11:11:41 +0000

    build

--- elan2mqtt/run.sh
+++ elan2mqtt/run.sh
@@ -1,27 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Run.sh starting"
-DIR=/home/pan/elan2mqtt/elan2mqtt/elan2mqtt
-CONFIG_PATH=/$DIR/config.json
-SYSTEM_USER=/$DIR/system_user.json
-
-ELAN_URL=$(jq --raw-output ".options.eLanURL" $CONFIG_PATH)
-MQTT_SERVER=$(jq --raw-output ".options.MQTTserver" $CONFIG_PATH)
-USERNAME=$(jq --raw-output ".options.username" $CONFIG_PATH)
-PASSWORD=$(jq --raw-output ".options.password" $CONFIG_PATH)
-LOGLEVEL=$(jq --raw-output ".options.log_level" $CONFIG_PATH)
-DISABLEAUTODISCOVERY=$(jq --raw-output ".options.disable_autodiscovery" $CONFIG_PATH)
-
-#echo "Installing requirements"
-#pip3 install -r requirements.txt
-
-#trap 'kill $PID' exit
+ELAN2MQTT_VERSION=1.16.0
+ARCHIVE=elan2mqtt-$ELAN2MQTT_VERSION
 
 echo "Starting gateway"
-echo elan url: ${ELAN_URL}
-echo mqtt server: ${MQTT_SERVER}
-echo "Loglevel:" ${LOGLEVEL} 
-echo "Autodiscovery disabled:" ${DISABLEAUTODISCOVERY}
-echo python3 main_worker.py ${ELAN_URL} ${MQTT_SERVER} -elan-user ${USERNAME} -elan-password ${PASSWORD} -log-level ${LOGLEVEL} -disable-autodiscovery ${DISABLEAUTODISCOVERY} &
-#PID=$!
-echo python3 socket_listener.py ${ELAN_URL} ${MQTT_SERVER} -elan-user ${USERNAME} -elan-password ${PASSWORD} -log-level ${LOGLEVEL} 
+export PYTHONPATH=/$ARCHIVE
+python3 main_worker.py -log-level trace -disable-autodiscovery false &
+python3 socket_listener.py -log-level trace
